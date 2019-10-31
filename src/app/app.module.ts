6c434f982e9e0083cf1cd94ab312658be9be06b2
ahmed abdelrhman <ahmedabdelrhman414@gmail.com>
2019-10-31 15:53:06 +0200

    install bootstrap ang login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ReportingComponent } from './reporting/reporting.component';
-
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +16,7 @@ import { ReportingComponent } from './reporting/reporting.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
